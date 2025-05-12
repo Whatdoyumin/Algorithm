b3f4e97443f3388dd25bfcb49ff893c84489c32f
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-05-12 22:54:42 +0900

    [level 2] Title: 카펫, Time: 10.58 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        
+        for(int i = 3; i < sum; i++) {
+            if(sum % i == 0) {
+                int j = sum / i;
+                if ((i - 2) * (j - 2) == yellow) {
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
