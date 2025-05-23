cf670ce56aa530627aef25046ae2cdf27afdf226
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-05-23 10:08:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.81 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,44 @@
 import java.util.*;
 
 class Solution {
+    int[] dr = {0, -1, 0, 1};
+    int[] dc = {1, 0, -1, 0};
+    int rowLength, colLength;
+    
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        
-        int[][] distance = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
+        rowLength = maps.length;
+        colLength = maps[0].length;
         
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0});
+        q.offer(new int[]{0, 0, 1});
+            
+        boolean[][] visited = new boolean[rowLength][colLength];
         visited[0][0] = true;
-        distance[0][0] = 1;
         
         while(!q.isEmpty()) {
-            int[] current = q.poll();
-            int x = current[0];
-            int y = current[1];
+            // 현재 노드 방문
+            int[] cur = q.poll();
+            int curRow = cur[0];
+            int curCol = cur[1];
+            int dist = cur[2];
             
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (maps[nx][ny] == 1 && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        distance[nx][ny] = distance[x][y] + 1;
-                        q.add(new int[]{nx, ny});
+            // if 도착지 도착하면 dist 반환
+            if(curRow == rowLength - 1 && curCol == colLength - 1) return dist;
+            
+            // 다음 노드 예약
+            for(int i = 0; i < 4; i++) {
+                int nextRow = curRow + dr[i];
+                int nextCol = curCol + dc[i];
+                if((nextRow >= 0 && nextRow < rowLength) && (nextCol >= 0 && nextCol < colLength)) {
+                    if(!visited[nextRow][nextCol] && maps[nextRow][nextCol] == 1) {
+                        visited[nextRow][nextCol] = true;
+                        q.offer(new int[]{nextRow, nextCol, dist+1});
                     }
-                }
+                } 
             }
         }
         
-        return distance[n-1][m-1] == 0 ? -1 : distance[n-1][m-1];
+        return -1;
+        
     }
 }
