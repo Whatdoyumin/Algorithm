976cf6ca10cd4a560136372ebb42b1b3c4d40773
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-05-29 17:45:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.92 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -11,34 +11,38 @@ class Solution {
         
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{0, 0, 1});
-            
+        
         boolean[][] visited = new boolean[rowLength][colLength];
         visited[0][0] = true;
         
         while(!q.isEmpty()) {
-            // 현재 노드 방문
             int[] cur = q.poll();
             int curRow = cur[0];
             int curCol = cur[1];
             int dist = cur[2];
             
-            // if 도착지 도착하면 dist 반환
-            if(curRow == rowLength - 1 && curCol == colLength - 1) return dist;
+            if(curRow == rowLength - 1 && curCol == colLength - 1) {
+                return dist;
+            }
             
-            // 다음 노드 예약
             for(int i = 0; i < 4; i++) {
                 int nextRow = curRow + dr[i];
                 int nextCol = curCol + dc[i];
-                if((nextRow >= 0 && nextRow < rowLength) && (nextCol >= 0 && nextCol < colLength)) {
-                    if(!visited[nextRow][nextCol] && maps[nextRow][nextCol] == 1) {
+                
+                if(isValid(nextRow, nextCol, maps)) {
+                    if(!visited[nextRow][nextCol]) {
                         visited[nextRow][nextCol] = true;
-                        q.offer(new int[]{nextRow, nextCol, dist+1});
+                        q.offer(new int[]{nextRow, nextCol, dist + 1});
                     }
-                } 
+                }
             }
         }
         
-        return -1;
         
+        return -1;
+    }
+    
+    boolean isValid(int r, int c, int[][] grid) {
+        return (r >= 0 && r < rowLength) && (c >= 0 && c < colLength) && grid[r][c] == 1;
     }
 }
