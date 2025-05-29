49d1182903244ae70d42d80f8db0d02ac3165462
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-05-29 16:41:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.20 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,47 +2,52 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-
-        for (int i = 1; i <= n; i++) {
-            graph.put(i, new ArrayList<>());
+        
+        // 연결된 트리 구조 만들기
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i <= n; i++) {
+            map.put(i, new ArrayList<>());
         }
-
-        for (int[] wire : wires) {
-            int a = wire[0], b = wire[1];
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+        
+        for(int[] wire : wires) {
+            int a = wire[0];
+            int b = wire[1];
+            
+            map.get(a).add(b);
+            map.get(b).add(a);
         }
-
-        int minDiff = Integer.MAX_VALUE;
-
-        // 간선 하나씩 끊으면서 최소 차이 계산
-        for (int[] wire : wires) {
-            int a = wire[0], b = wire[1];
-
-            graph.get(a).remove(Integer.valueOf(b));
-            graph.get(b).remove(Integer.valueOf(a));
-
+        
+        int answer = Integer.MAX_VALUE;
+        
+        // 간선 하나씩 끊어보면서 최소값 갱신
+        for(int[] wire : wires) {
+            int a = wire[0];
+            int b = wire[1];
+            
+            map.get(a).remove(Integer.valueOf(b));
+            map.get(b).remove(Integer.valueOf(a));
+            
             boolean[] visited = new boolean[n + 1];
-            int count = dfs(a, graph, visited);
-
-            minDiff = Math.min(minDiff, Math.abs(n - 2 * count));
-
-            // 복원
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            // dfs로 한 쪽 노드 개수 구하기
+            int count = dfs(a, map, visited);
+            
+            answer = Math.min(answer, Math.abs(n - 2 * count));
+            
+            // 다시 간선 붙이기
+            map.get(a).add(b);
+            map.get(b).add(a);
         }
-
-        return minDiff;
+        
+        return answer;
     }
-
-    private int dfs(int node, Map<Integer, List<Integer>> graph, boolean[] visited) {
-        visited[node] = true;
+    
+    int dfs(int start, Map<Integer, List<Integer>> graph, boolean[] visited) {
+        visited[start] = true;
         int count = 1;
-
-        for (int neighbor : graph.get(node)) {
-            if (!visited[neighbor]) {
-                count += dfs(neighbor, graph, visited);
+        
+        for(int node : graph.get(start)) {
+            if(!visited[node]) {
+                count += dfs(node, graph, visited);
             }
         }
         return count;
