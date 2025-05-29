0bd0b5bd5eaf16eb74dda0b4de92b7f83758ff47
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-05-30 00:54:43 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.96 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,38 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
         
-        // 연결된 트리 구조 만들기
+        // 연결된 트리 생성
         Map<Integer, List<Integer>> map = new HashMap<>();
         for(int i = 0; i <= n; i++) {
             map.put(i, new ArrayList<>());
         }
         
+        // 연결
         for(int[] wire : wires) {
-            int a = wire[0];
+            int a = wire[0]; 
             int b = wire[1];
             
             map.get(a).add(b);
             map.get(b).add(a);
         }
         
-        int answer = Integer.MAX_VALUE;
-        
-        // 간선 하나씩 끊어보면서 최소값 갱신
+        // 간선 하나씩 끊으면서 살펴보기
         for(int[] wire : wires) {
             int a = wire[0];
             int b = wire[1];
             
+            // 간선 끊기
             map.get(a).remove(Integer.valueOf(b));
             map.get(b).remove(Integer.valueOf(a));
             
             boolean[] visited = new boolean[n + 1];
-            // dfs로 한 쪽 노드 개수 구하기
             int count = dfs(a, map, visited);
             
-            answer = Math.min(answer, Math.abs(n - 2 * count));
+            answer = Math.min(answer, Math.abs(2 * count - n));
             
-            // 다시 간선 붙이기
+            // 다시 이어 붙이기
             map.get(a).add(b);
             map.get(b).add(a);
         }
@@ -41,14 +41,14 @@ class Solution {
         return answer;
     }
     
-    int dfs(int start, Map<Integer, List<Integer>> graph, boolean[] visited) {
+    // 연결된 애들 개수 구하기
+    int dfs(int start, Map<Integer, List<Integer>> map, boolean[] visited) {
         visited[start] = true;
         int count = 1;
         
-        for(int node : graph.get(start)) {
-            if(!visited[node]) {
-                count += dfs(node, graph, visited);
-            }
+        for(int node : map.get(start)) {
+            if(!visited[node]) 
+                count += dfs(node, map, visited);
         }
         return count;
     }
