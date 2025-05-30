cc6b289321811a43ffe501dd9dce539818c7a4ae
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-05-30 13:18:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.95 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,53 +2,51 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        
-        // 연결된 트리 생성
+        // 트리 형태 만들기
         Map<Integer, List<Integer>> map = new HashMap<>();
         for(int i = 0; i <= n; i++) {
             map.put(i, new ArrayList<>());
         }
-        
-        // 연결
-        for(int[] wire : wires) {
-            int a = wire[0]; 
-            int b = wire[1];
-            
-            map.get(a).add(b);
-            map.get(b).add(a);
-        }
-        
-        // 간선 하나씩 끊으면서 살펴보기
+        //연결
         for(int[] wire : wires) {
             int a = wire[0];
             int b = wire[1];
             
-            // 간선 끊기
-            map.get(a).remove(Integer.valueOf(b));
-            map.get(b).remove(Integer.valueOf(a));
-            
-            boolean[] visited = new boolean[n + 1];
-            int count = dfs(a, map, visited);
-            
-            answer = Math.min(answer, Math.abs(2 * count - n));
-            
-            // 다시 이어 붙이기
             map.get(a).add(b);
             map.get(b).add(a);
         }
         
+        int answer = Integer.MAX_VALUE;
+        
+        // 간선 끊어보며 확인
+        for(int[] wire : wires) {
+            int a = wire[0];
+            int b = wire[1];
+            
+            map.get(a).remove(Integer.valueOf(b));
+            map.get(b).remove(Integer.valueOf(b));
+            
+            boolean[] visited = new boolean[n + 1];
+            int count = dfs(a, map, visited);
+            
+            answer = Math.min(answer, Math.abs(count * 2 - n));
+            
+            map.get(a).add(b);
+            map.get(b).add(a);
+        }
+        
+        
         return answer;
     }
     
-    // 연결된 애들 개수 구하기
     int dfs(int start, Map<Integer, List<Integer>> map, boolean[] visited) {
-        visited[start] = true;
+       visited[start] = true;
         int count = 1;
         
         for(int node : map.get(start)) {
-            if(!visited[node]) 
+            if(!visited[node]) {
                 count += dfs(node, map, visited);
+            }
         }
         return count;
     }
