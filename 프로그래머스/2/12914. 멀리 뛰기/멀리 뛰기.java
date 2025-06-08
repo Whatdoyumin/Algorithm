3cafc1b42e15edc64241ccf2442feb4018c50cf0
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-06-08 13:20:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -4,9 +4,10 @@ class Solution {
         long[] dp = new long[n + 1];
         
         dp[1] = 1;
-        if(n >= 2) dp[2] = 2;
+        if(n >= 2)
+            dp[2] = 2;
         
-        for(int i = 3; i <= n; i++) {
+        for(int i = 3; i < n + 1; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
         
