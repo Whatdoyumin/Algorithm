33b56baef9352be2453f543b1c4ff19137fb063a
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-06-09 17:21:40 +0900

    [level 3] Title: 등굣길, Time: 0.65 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        boolean[][] isPuddle = new boolean[m + 1][n + 1];
+        for (int[] p : puddles) {
+            isPuddle[p[0]][p[1]] = true;
+        }
+
+        dp[1][1] = 1;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (isPuddle[i][j]) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if (i == 1 && j == 1) continue;
+
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+            }
+        }
+
+        return dp[m][n];
+    }
+}
