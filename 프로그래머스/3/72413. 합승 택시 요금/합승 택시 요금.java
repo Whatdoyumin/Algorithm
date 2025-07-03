0ce7d8c53e3e494a82e5bfb613fbec942a828252
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-07-03 18:58:42 +0900

    [level 3] Title: 합승 택시 요금, Time: 122.47 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,42 +1,37 @@
 import java.util.*;
 
 class Solution {
-    static final int INF = 200_000_000;
-
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int[][] dist = new int[n + 1][n + 1];
-
-        // 초기화
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;
+        int answer = 0;
+        
+        // s, a, b 3개의 노드에서 모든 노드까지 도달하는 최소 비용 저장
+        int[][] map = new int[n + 1][n + 1];
+        for(int i = 1; i <= n; i++) {
+            Arrays.fill(map[i], 200 * 100_000);
+            map[i][i] = 0;
         }
-
-        // 요금 테이블 적용
-        for (int[] fare : fares) {
-            int u = fare[0], v = fare[1], cost = fare[2];
-            dist[u][v] = cost;
-            dist[v][u] = cost;
+        
+        for(int[] fare : fares) {
+            map[fare[0]][fare[1]] = fare[2];
+            map[fare[1]][fare[0]] = fare[2];
         }
-
-        // 플로이드 워셜 활용
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
+        
+        for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
                     }
                 }
             }
         }
-
-        // 경유하는 지점마다 비용 계산
-        int answer = INF;
-        for (int k = 1; k <= n; k++) {
-            int total = dist[s][k] + dist[k][a] + dist[k][b];
-            answer = Math.min(answer, total);
+        
+        answer = map[s][a] + map[s][b];
+        
+        for(int i = 1; i <= n; i++) {
+            answer = Math.min(answer, map[s][i] + map[i][a] + map[i][b]);
         }
-
+        
         return answer;
     }
 }
