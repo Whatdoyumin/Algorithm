cbabceb42f58f1577f1cb833f8b8a92da4e7b5ca
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-07-04 01:35:22 +0900

    Time: 0 ms (100%), Space: 41.3 MB (35.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,7 +5,7 @@ class Solution {
         if(n == 0) return 0;
         if(n == 1) return nums[0];
 
-        int[] dp = new int[n];
+        int[] dp = new int[n + 1];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
 
