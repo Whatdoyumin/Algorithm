507f70e216ba1f2d0bfa31fd23d7460619094105
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2025-07-10 23:24:16 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(computers, visited, i);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    void dfs(int[][] computers, boolean[] visited, int cur) {
+        visited[cur] = true;
+        
+        for(int i = 0; i < computers.length; i++) {
+            if(!visited[i] && computers[cur][i] == 1) {
+                dfs(computers, visited, i);
+            }
+        }
+    }
+}
