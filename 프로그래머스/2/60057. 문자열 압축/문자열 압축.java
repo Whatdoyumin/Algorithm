1a146ebc786ce1eaacf99a2215c8888d93dc3aa3
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2026-02-18 15:11:29 +0900

    [level 2] Title: 문자열 압축, Time: 2.13 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,36 +1,46 @@
-public class Solution {
+class Solution {
     public int solution(String s) {
+        // 1개 이상 단위로 문자열을 잘라 압축한 것 중 가장 짧은 길이 return (최솟값) -> 그리디?
+        // 문자의 개수와 반복되는 값으로 표현하여 압축
+        // aabbaccc -> 2a2ba3c
+        // ababcdcdababcdcd -> 2ab2cd2ab2cd
+        
+        int answer = Integer.MAX_VALUE;
+        
         int minLength = s.length();
-
-        for (int step = 1; step <= s.length() / 2; step++) {
-            StringBuilder compressed = new StringBuilder();
-            String prev = s.substring(0, step);
+        
+        // 1글자씩 잘라서 세보기 -> 글자수 늘려서 min 찾기
+        // i : 자르는 글자수?
+        for(int i = 1; i <= minLength; i++) {
+            String prev = s.substring(0, 0 + i);
             int count = 1;
-
-            for (int j = step; j < s.length(); j += step) {
-                int end = Math.min(j + step, s.length());
-                String current = s.substring(j, end);
-
-                if (current.equals(prev)) {
+            
+            // 그 압축된 글자 자체를 추출한 후 글자수 세지 않고
+            // 바로 글자 수만 계산
+            int resultLength = 0;
+            
+            for(int j = i; j + i <= minLength; j += i) {
+                // 중복되는지 판단할 글자
+                String cur = s.substring(j, j + i);
+                
+                // 현재 글자가 중복 판단 글자와 같으면 
+                if (cur.equals(prev)) {
                     count++;
                 } else {
-                    if (count > 1) {
-                        compressed.append(count);
-                    }
-                    compressed.append(prev);
-                    prev = current;
+                    // 이전 글자와 중복 없으면 이전 글자 길이 누적
+                    resultLength += (count > 1 ? String.valueOf(count).length() + prev.length() : prev.length());
+                    prev = cur;
                     count = 1;
                 }
             }
-
-            if (count > 1) {
-                compressed.append(count);
-            }
-            compressed.append(prev);
-
-            minLength = Math.min(minLength, compressed.length());
+            
+            if(minLength % i != 0) resultLength += minLength % i;
+            
+            resultLength += (count > 1 ? String.valueOf(count).length() + prev.length() : prev.length());
+            answer = Math.min(answer, resultLength);
         }
-
-        return minLength;
+        
+        
+        return answer;
     }
 }
