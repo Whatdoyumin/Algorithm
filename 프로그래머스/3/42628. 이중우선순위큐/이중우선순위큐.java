f8b1125d2a8679bc9e7f5a0683e8d8d8c4ded7ac
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2026-02-19 14:40:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 51.78 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,32 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        // 연산이 주어지면, 모든 연산 처리 후
+        // 큐가 비어 잇으면 [0, 0], 비어있지 않으면 [최댓값, 최솟값]
+        
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
-
-        for (String op : operations) {
-            String[] parts = op.split(" ");
-            String command = parts[0];
-            int value = Integer.parseInt(parts[1]);
-
-            if (command.equals("I")) {
-                minHeap.add(value);
-                maxHeap.add(value);
-            } else if (command.equals("D")) {
-                if (minHeap.isEmpty()) continue;
-
-                if (value == 1) { 
-                    int max = maxHeap.poll();
-                    minHeap.remove(max);
-                } else {
-                    int min = minHeap.poll();
-                    maxHeap.remove(min);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(String o : operations) {
+            if (o.startsWith("I")) {
+                String num = o.substring(2, o.length());
+                minHeap.add(Integer.parseInt(num));
+                maxHeap.add(Integer.parseInt(num));
+            } else if (o.equals("D 1")) {
+                if (!maxHeap.isEmpty()) {
+                    int n = maxHeap.poll();
+                    minHeap.remove(n);
+                }
+            } else if (o.equals("D -1")) {
+                if (!minHeap.isEmpty()) {
+                    int n = minHeap.poll();
+                    maxHeap.remove(n);
                 }
             }
         }
-
-        if (minHeap.isEmpty()) return new int[]{0, 0};
-
+        
+        if (minHeap.isEmpty() && maxHeap.isEmpty()) {
+            return new int[]{0, 0};
+        }
+        
         return new int[]{maxHeap.peek(), minHeap.peek()};
     }
 }
