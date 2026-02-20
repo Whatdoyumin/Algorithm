d5840a5e283ad902b47260a498a1228a64772add
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2026-02-21 03:34:21 +0900

    [level 2] Title: 타겟 넘버, Time: 9.15 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,21 @@
 class Solution {
+    static int[] arr;
+    
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, target, 0, 0);
+        // 반복해서 확인하므로 재귀 -> dfs
+        int sum = 0;
+        arr = numbers;
+        
+        return dfs(0, sum, target);
     }
     
-    int dfs(int[] numbers, int target, int index, int cur) {
-        if(index == numbers.length) {
-            return (cur == target) ? 1 : 0;
+    public int dfs(int i, int sum, int target) {
+        if (i == arr.length) {
+            if (sum == target) return 1;
+            else return 0;
         }
         
-        int sum = 0;
-        sum += dfs(numbers, target, index + 1, cur + numbers[index]);
-        sum += dfs(numbers, target, index + 1, cur - numbers[index]);
-        
-        return sum;
+        return dfs(i+1, sum + arr[i], target)
+                + dfs(i+1, sum - arr[i], target);
     }
 }
