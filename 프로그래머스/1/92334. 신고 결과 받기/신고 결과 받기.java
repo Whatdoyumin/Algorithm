41aa684354071bf7272099e455cdfb5cdb9091d8
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2026-04-13 15:33:27 +0900

    [level 1] Title: 신고 결과 받기, Time: 187.65 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,40 +1,38 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
-        // 중복 신고 방지를 위한 set
-        Set<String> set = new HashSet<>(Arrays.asList(report));
+        Map<String, HashSet<String>> reporter = new HashMap<>();
+        Map<String, Integer> reportee = new HashMap<>();
+        HashSet<String> set;
         
-        // map 선언
-        Map<String, Integer> count = new HashMap<>();
-        Map<String, List<String>> reportWho = new HashMap<>();
-        
-        // map에 값 넣기
-        for(String s : set) {
-            String[] content = s.split(" ");
-            String from = content[0];
-            String to = content[1];
-            
-            // count값 넣기
-            count.put(to, count.getOrDefault(to, 0) + 1);
-            
-            // 신고한 사람의 신고 리스트 넣기
-            // 만약 이전에 신고한 적이 없으면 기본값 넣기
-            if(!reportWho.containsKey(from))
-                reportWho.put(from, new ArrayList<>());
-            reportWho.get(from).add(to);
+        for(String name : id_list) {
+            reporter.put(name, new HashSet<>());
+            reportee.put(name, 0);
         }
         
-        // 신고한 사람 기준으로 신고당한 사람의 신고횟수가 k 이상일 경우 answer에 추가
-        for(int i = 0; i < id_list.length; i++) {
-            // 신고한 사람
-            String reporter = id_list[i];
+        for(String r : report) {
+            StringTokenizer st = new StringTokenizer(r);
+            String from = st.nextToken();
+            String to = st.nextToken();
             
-            for(String reportee : reportWho.getOrDefault(reporter, new ArrayList<>())) {
-                if(count.get(reportee) >= k)
+            set = reporter.get(from);
+            if(set.add(to)) {
+                reportee.put(to, reportee.get(to) + 1);
+            }
+        }
+        
+        for(int i = 0; i < id_list.length; i++) {
+            String name = id_list[i];
+            
+            set = reporter.get(name);
+            for(String s : set) {
+                if (reportee.get(s) >= k) {
                     answer[i] += 1;
+                }
             }
         }
         
