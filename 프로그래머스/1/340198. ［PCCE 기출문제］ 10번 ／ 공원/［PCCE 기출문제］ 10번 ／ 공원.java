564698dd7173867fb4ff1ab59f5121c94f7c1bcf
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2026-06-25 12:41:52 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 4.51 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        Arrays.sort(mats);
+
+        for (int i = mats.length - 1; i >= 0; i--) {
+            int size = mats[i];
+
+            for (int r = 0; r <= park.length - size; r++) {
+                for (int c = 0; c <= park[0].length - size; c++) {
+                    if (canPlace(park, r, c, size)) {
+                        return size;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private boolean canPlace(String[][] park, int r, int c, int size) {
+        for (int i = r; i < r + size; i++) {
+            for (int j = c; j < c + size; j++) {
+                if (!park[i][j].equals("-1")) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
