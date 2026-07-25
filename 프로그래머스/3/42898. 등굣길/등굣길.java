6afad401fa01bed88dcc4f8fdcf407cd42e22529
YUMIN LEE <90082215+Whatdoyumin@users.noreply.github.com>
2026-07-25 10:36:52 +0900

    [level 3] Title: 등굣길, Time: 0.37 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,22 +1,29 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[m + 1][n + 1];
+        // dp로 풀래요
+        
+        // dp[i][j] = (i, j)로 오는 경우의 수
+        // 근데 이걸 어떻게 생각해내는거냐고
         
         boolean[][] isPuddle = new boolean[m + 1][n + 1];
         for(int[] puddle : puddles) {
-            isPuddle[puddle[0]][puddle[1]] = true;
+            int a = puddle[0];
+            int b = puddle[1];
+            
+            isPuddle[a][b] = true;
         }
         
-        dp[1][1] = 1;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][1] = 1;       
+        
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
-                if(isPuddle[i][j]) {
+                if (isPuddle[i][j]) {
                     dp[i][j] = 0;
                     continue;
                 }
                 
-                if(i == 1 && j == 1)
-                    continue;
+                if (i == 1 && j == 1) continue;
                 
                 dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
             }
